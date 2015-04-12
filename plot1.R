7f59e864160303f66557e5536cf6a3db6ade30f9
Nohtal <party.in.the.sky@gmail.com>
2015-04-12 19:16:00 -0400

    trying to work this thing...

---
+++ plot1.R
@@ -0,0 +1,20 @@
+dataj <-read.table('household_power_consumption.txt',header = TRUE,sep = ";",na.strings = "?")
+attach(dataj)
+names(dataj)
+head(dataj)
+
+data<-dataj[complete.cases(dataj),]
+
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+
+
+thursday<-(data$Date=="2007-02-01")
+friday<-(data$Date=="2007-02-02")
+t.data<-data[thursday,]
+f.data<-data[friday,]
+days<-rbind(t.data,f.data)
+
+
+
+#creating plot 1
+hist(days$Global_active_power,col = 'red',xlab = 'Global Active Power (kilowatts)',main = "Global Active Power")
---
+++ plot2.R
@@ -0,0 +1,23 @@
+dataj <-read.table('household_power_consumption.txt',header = TRUE,sep = ";",na.strings = "?")
+attach(dataj)
+names(dataj)
+head(dataj)
+
+data<-dataj[complete.cases(dataj),]
+
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+
+
+thursday<-(data$Date=="2007-02-01")
+friday<-(data$Date=="2007-02-02")
+t.data<-data[thursday,]
+f.data<-data[friday,]
+days<-rbind(t.data,f.data)
+
+
+
+#creating plot 2
+days$Date<-format(days$Date,"%A %B %Y")
+bdays = c('Thu','Fri','Sat')
+plot(days$Global_active_power,type ='l',xaxt ='n',ylab = 'Global Active Power (kilowatts)')
+axis(1,at = c(0,1500,2880),length(days$Date),labels = bdays)
