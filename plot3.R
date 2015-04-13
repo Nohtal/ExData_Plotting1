77ab6b4fde48624977025452e54849439ba86783
Nohtal <party.in.the.sky@gmail.com>
2015-04-12 20:38:06 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -14,9 +14,11 @@ t.data<-data[thursday,]
 f.data<-data[friday,]
 days<-rbind(t.data,f.data)
 
+png('plot3.png')
 #creating plot 3
 plot(days$Sub_metering_1,type ='l',xaxt ='n',ylab = 'Global Active Power (kilowatts)')
 lines(days$Sub_metering_2,xaxt = 'n',col= 'red')
 lines(days$Sub_metering_3,xaxt = 'n',col = 'blue')
 axis(1,at = c(0,1500,2880),length(days$Date),labels = bdays)
 legend('topright',lty = c(1,1,1),col = c('black','red','blue'),legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
+dev.off()
